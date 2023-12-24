c6e29c10b183e3ac2d4f98a7ea8ac429a2da8986
Riyan Ramadhan <ramadhanriyan3@gmail.com>
2023-12-25 05:59:49 +0700

    fix(rent-car): fix typo

--- frontend/src/components/accordion/accordion.tsx
+++ frontend/src/components/accordion/accordion.tsx
@@ -17,7 +17,7 @@ function FaqAccordion() {
               <Accordion.Header>Frequently Asked Question</Accordion.Header>
               <Accordion.Body>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                eiusmod tempor incididunt ut labore et magna aliqua.
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1" className="mb-3">
